873cc365551ae2889d83b5da6f3797d504e3ab3b
kscarpellino <113559703+kscarpellino@users.noreply.github.com>
2024-02-22 17:28:25 -0500

    created joke.cpp and joke.h and main.cpp

---
+++ joke.h
@@ -0,0 +1,28 @@
+//
+// Created by Kylie Scarpellino on 2/22/24.
+//
+
+#ifndef M2_OEP_JBOOTHHO_KSCARPELLINO_JOKE_H
+#define M2_OEP_JBOOTHHO_KSCARPELLINO_JOKE_H
+
+
+#include <string>
+
+class Joke {
+public:
+    // Constructor
+    Joke(std::string type, std::string setup, std::string punchline);
+
+    // Getter methods
+    std::string getType() const;
+    std::string getSetup() const;
+    std::string getPunchline() const;
+
+private:
+    std::string type;      // Type of joke (e.g., Knock Knock, Pun, Riddle)
+    std::string setup;     // Setup or lead-in of the joke
+    std::string punchline; // Punchline or the humorous ending of the joke
+};
+
+
+#endif //M2_OEP_JBOOTHHO_KSCARPELLINO_JOKE_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Kylie Scarpellino on 2/22/24.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
